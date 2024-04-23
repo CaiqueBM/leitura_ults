0282882f7677f25b87e31039df3fe29cb3f53649
caiquemaso <caiquebmaso@ucl.br>
2024-04-23 11:56:28 -0300

    Buscar dados do dia anterior

--- op_diaria/buscar_dados.py
+++ op_diaria/buscar_dados.py
@@ -1,10 +1,12 @@
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 import pandas as pd
 import sqlite3
 
 dfdados = pd.DataFrame()
-data_atual = datetime.now().strftime("%Y-%m-%d")
+data_atual = datetime.now()
+data_anterior = data_atual - timedelta(days=1)
+data_anterior = data_anterior.strftime("%Y-%m-%d")
 
 """ids = [578496, 667618]
 ult = {578496: "ULT 1", 667618: "ULT 3"}"""
@@ -13,7 +15,7 @@ ids = [578496, 667618, 686932, 767468]
 ult = {578496: "ULT 1", 667618: "ULT 3", 686932: "ULT 4", 767468: "ULT 2"}
 
 for usina_id in ids:
-    url = f"https://app.solarz.com.br/shareable/generation/period?usinaId={usina_id}&start={data_atual}&end={data_atual}&uniteMonths=false"
+    url = f"https://app.solarz.com.br/shareable/generation/period?usinaId={usina_id}&start={data_anterior}&end={data_anterior}&uniteMonths=false"
 
     # Realiza a requisição GET para obter os dados da página
     response = requests.get(url)
@@ -30,7 +32,7 @@ for usina_id in ids:
 
     df_usina = pd.DataFrame(
         {
-            "DATA": data_atual,
+            "DATA": data_anterior,
             "GERACAO": total_gerado,
             "ULT": nome_ult,
         },
